0c9d204d0dc2a90ae47afcbb29ab4b72867bc85a
Md. Rifat Reza <014rjakash@gmail.com>
2024-01-25 11:48:17 +0600

    new commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,4 +3,10 @@ const list = document.querySelector(".itames")
 
 pressIcon.addEventListener("click", function(){
     list.classList.toggle("showData")
+
+    if (list.classList.contains("showData")){
+        document.getElementById("bar").className = "bi bi-x-lg"
+    }else{
+        document.getElementById("bar").className = "bi bi-list"
+    }
 })
